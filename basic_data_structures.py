aac9f4f15d2a9dd2bd978d0104928fa7b154b15e
Mehmet nuri yilmaz 34763 <mehmet.cage@hotmail.com>
2025-03-11 11:17:44 +0100

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,23 @@
+# Integer
+my_age = 22  
+
+# Floating point
+pi_value = 3.14  
+
+# String
+greeting = "Hello"  
+
+# List
+numbers_list = [10, "hello", 200, 3]  
+
+# Dictionary
+user_info = {"mykey": "value", "name": "Mehmet"}  
+
+# Tuple
+coordinates = (10, "hello", 200, 3)  
+
+# Set
+unique_letters = {"a", "b"}  # set
+
+# Boolean
+is_python_fun = True  
