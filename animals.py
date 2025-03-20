f221712f25d9233a63d8b03cea9148eb7f3a2137
Mehmet nuri yilmaz 34763 <mehmet.cage@hotmail.com>
2025-03-20 12:07:04 +0000

    new changes

--- animals.py
+++ animals.py
@@ -1,1 +1,12 @@
 
+       
+Animals1={"name":"crocodile" , "group": "Reptile" , "number of legs":"4" , "Skills": "strong bite"} 
+Animals2={"name":"Cat" , "group":"mammals" , "number of legs":"4" , "Skills": "sleeping"}
+Animals3={"name":"parrot" , "group":"birds" , "number of legs": "2" , "Skills": "Speak"}
+Animals4={"name":"bear" , "group":"mammals", "number of legs": "4" , "Skills": "Climbing"}
+Animals5={"name":"zebras" , "group":"mammals" , "number of legs":"4" , "Skills": "fast running"}
+print(Animals1["name"],Animals1["group"],Animals1["number of legs"],Animals1["Skills"])
+print(Animals2["name"],Animals2["group"],Animals2["number of legs"],Animals2["Skills"])
+print(Animals3["name"],Animals3["group"],Animals3["number of legs"],Animals3["Skills"])
+print(Animals4["name"],Animals4["group"],Animals4["number of legs"],Animals4["Skills"])
+print(Animals5["name"],Animals5["group"],Animals5["number of legs"],Animals5["Skills"])
